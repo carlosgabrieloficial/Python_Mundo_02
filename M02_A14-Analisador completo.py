74f52c365fff401007460534e3569ab27f77eed5
Carlos Gabriel Sousa <carlos.desenvolvedor.dev.@gmail.com>
2025-08-24 17:40:23 -0300

    update

--- M02_A14-Analisador completo.py
+++ M02_A14-Analisador completo.py
@@ -2,6 +2,10 @@
 
 soma_idade = 0 
 media_idade = 0
+maioridade_homem = 0 
+nome_homemvelho = 0
+total_mulheres = 0 
+
 
 for paciente in range (1,5):
     
@@ -10,8 +14,23 @@ for paciente in range (1,5):
     idade = int(input('Por favor,informe a sua idade :'))
     soma_idade += idade
     sexo = str(input('Sexo [M] / [F] :')).strip()
+   
+    if sexo in "Ff" and idade > 20 :
+        total_mulheres += 1
+
+    if paciente == 1 and sexo in "Mm":
+        nome_homemvelho = nome
+        maioridade_homem = idade
+
+    if sexo in "Mm" and idade > maioridade_homem:
+        maioridade_homem = idade
+        nome_homemvelho = nome
 
 
 media_idade = soma_idade / 4
 
-print(media_idade)
+print(f"A media entre todos os paciantes é {media_idade}")
+
+print(f'O homem mais velho é o {nome_homemvelho} e a sua idade é {maioridade_homem}')
+
+print(f'Tem o total de {total_mulheres} mulheres acima dos 20 anos')
